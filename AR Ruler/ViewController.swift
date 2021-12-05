e877ac0b5c84040719903cbad04070dda56bd15a
Yasser Hajlaoui <yasser159@hotmail.com>
2021-12-04 19:23:31 -0800

    added icon

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -44,7 +44,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        print("Touch Detected")
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
         
         if let touchLocation = touches.first?.location(in: sceneView){
             
@@ -72,7 +78,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.scene.rootNode.addChildNode(dotNode)
         
-        dotNode.append(dotNode)
+        dotNodes.append(dotNode)
         
         if dotNodes.count >= 2 {
             calculate()
@@ -83,7 +89,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let start = dotNodes[0]
         let end   = dotNodes[1]
         
+        print(start.position)
+        print(end.position)
+        
+
+        let distance = sqrt(pow(end.position.x - start.position.x, 2) +
+                            pow(end.position.y - start.position.y, 2) +
+                            pow(end.position.z - start.position.z, 2))
+        
+        //        print(abs (distance))
+        updateText(text: "\(abs(distance))",atPosition: end.position)
+        
+        //        distance = √ ((x2-x1)^2 + (y2+y1)^2 + (z2-z1)^2)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3){
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        let textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
         
     }
+    
     
 }
