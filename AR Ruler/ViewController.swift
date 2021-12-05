1da2dc48b0efab23ff5c4606b2983ea6e41154b5
Yasser Hajlaoui <yasser159@hotmail.com>
2021-12-04 19:34:19 -0800

    project finished

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -105,11 +106,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func updateText(text: String, atPosition position: SCNVector3){
         
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
-        
-        let textNode = SCNNode(geometry: textGeometry)
+
+        textNode = SCNNode(geometry: textGeometry)
         
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         
